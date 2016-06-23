a90e79055a03475dd933aeee0e795df3a2c2152c
jaysonslater <jaysonslater@jaysons-mbp>
2016-06-22 21:48:51 -0700

    made changes for thread.

---
+++ CIT360topic/src/httpcommunication/Threads.java
@@ -0,0 +1,67 @@
+package httpcommunication;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+public class Threads {
+
+ public static void main(String args[]) {
+     
+     ExecutorService executor = Executors.newCachedThreadPool();
+
+     // create thread
+     FutureTask task = new FutureTask(new SomeThread(false));
+     executor.submit(task); 
+     executor.shutdown();
+
+     try {
+         
+         executor.awaitTermination(3, TimeUnit.SECONDS);
+        
+         System.out.println("returns: " + task.get());
+     } catch (InterruptedException e) {
+         e.printStackTrace();
+     } catch (ExecutionException e) {
+         e.printStackTrace();
+     }
+
+     executor = Executors.newCachedThreadPool();
+     task = new FutureTask(new SomeThread(true));
+     executor.submit(task);
+
+     executor.shutdown();
+     try {
+         executor.awaitTermination(3, TimeUnit.SECONDS);
+         
+         System.out.println("returns: " + task.get());
+     } catch (InterruptedException e) {
+         e.printStackTrace();
+     } catch (ExecutionException e) {
+         e.printStackTrace();
+     }
+ } 
+
+ static class SomeThread implements Callable {
+     boolean throwError = false;
+
+     public SomeThread(boolean throwError) {
+         this.throwError = throwError;
+     }
+
+     public String call() throws Exception {
+         System.out.println("going..");
+         Thread.sleep(2000);
+         System.out.println("done");
+
+         if (throwError) {
+             throw new Exception("boom, exception");
+         }
+
+         return "value";
+     }
+ } 
+}
